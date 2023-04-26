85803d0b0cf7078d7523ce8543949855e01272ba
Kevin-Hayes <kevin.hayes@marquette.edu>
2023-04-26 17:18:50 -0500

    Created the Customer class.

---
+++ SpringProject/Model/Customer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpringProject.Model
+{
+    public class Customer
+    {
+        private static int autoIncreament;
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Customer()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+        }
+    }
+
+
+    public class Appointment
+    {
+        private static int autoIncreament;
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+    }
+
+
+    public class CustomerAppointment
+    {
+        public Customer customer { get; set; }
+
+        public Appointment appointment { get; set; }
+
+
+        public CustomerAppointment(Customer c, Appointment a)
+        {
+            customer = c;
+            appointment = a;
+        }
+    }
+
+
+    public class Customers
+    {
+
+        public List<Customer> customers { get; set;}
+        
+        public Customers()
+        {
+            customers = new List<Customer>();
+        }
+
+        public Customer Authenticate(string username, string password)
+        {
+            Customer c = customers.Where(o => o.UserName == username).First();
+            if(c != null)
+            {
+                if(c.Password == password)
+                {
+                    return c;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+    }
+
+}
